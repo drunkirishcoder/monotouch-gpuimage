ee4da92ec40360f31bb580492c1603e8733f8aab
drunkirishcoder <daniel.jin+git@gmail.com>
2013-08-20 14:07:36 -0400

    Initial base solution

---
+++ binding/ApiDefinition.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Drawing;
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.GpuImage
+{
+}
+
---
+++ binding/StructsAndEnums.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace MonoTouch.GpuImage
+{
+}
+
