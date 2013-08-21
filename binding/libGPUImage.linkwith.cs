c3f4fb755520712a37082c71d67f8144528e6f80
drunkirishcoder <daniel.jin+git@gmail.com>
2013-08-21 09:12:31 -0400

    Declare core framework dependencies

--- binding/libGPUImage.linkwith.cs
+++ binding/libGPUImage.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libGPUImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libGPUImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, Frameworks = "CoreMedia CoreVideo OpenGLES AVFoundation QuartzCore", ForceLoad = true)]
